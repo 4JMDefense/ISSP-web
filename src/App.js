b95347f80fab13b3c72dcb8b2c5920468215d5d4
aaronblinks <aaronmarcalas@gmail.com>
2024-03-21 10:47:05 -0700

    Implemented for-loop to reduce repetitive code blocks

--- src/App.js
+++ src/App.js
@@ -1,217 +1,68 @@
-import { useState } from "react";
+import { useState, useEffect } from 'react';
 import "./App.css";
 import LineChart from "./components/LineChart";
 import { UserData } from "./Data";
 
-
 function findHighestValues(data) {
   const highestValues = {};
-
-  // Iterate over each object in the array
   data.forEach(obj => {
-    // Iterate over each property in the object
     Object.keys(obj).forEach(key => {
-      // Update highest value for each property
       if (!(key in highestValues) || obj[key] > highestValues[key]) {
         highestValues[key] = obj[key];
       }
     });
   });
-
   return highestValues;
 }
 
-  // contains all the values of the highest values
-  const highestValues = findHighestValues(UserData);
-
-
+const chartsInfo = [
+  { key: 'altitude', label: 'Flight Data Altitude' },
+  { key: 'xaccel', label: 'Flight Data X Acceleration' },
+  { key: 'yaccel', label: 'Flight Data Y Acceleration' },
+  { key: 'zaccel', label: 'Flight Data Z Acceleration' },
+  { key: 'elevator', label: 'Flight Data Elevator' },
+  { key: 'aileron', label: 'Flight Data Aileron' },
+  { key: 'rudder', label: 'Flight Data Rudder' }
+];
 
 function App() {
+  const [chartData, setChartData] = useState({});
 
-  const [altitudeData, setAltitudeData] = useState({
-    
-    labels: UserData.map((data) => data.time),
-    datasets: [
-      {
-        label: "Flight Data Altitude",
-        data: UserData.map((data) => data.altitude),
-        backgroundColor: [
-          "rgba(75,192,192,1)",
-          "#ecf0f1",
-          "#50AF95",
-          "#f3ba2f",
-          "#2a71d0",
-        ],
-        borderColor: "black",
-        borderWidth: 2,
-      },
-    ],
-    
-  });
+  useEffect(() => {
+    const initialChartData = chartsInfo.reduce((acc, { key, label }) => {
+      acc[key] = {
+        labels: UserData.map(data => data.time),
+        datasets: [{
+          label,
+          data: UserData.map(data => data[key]),
+          backgroundColor: ["rgba(75,192,192,1)", "#ecf0f1", "#50AF95", "#f3ba2f", "#2a71d0"],
+          borderColor: "black",
+          borderWidth: 2,
+        }]
+      };
+      return acc;
+    }, {});
+    setChartData(initialChartData);
+  }, []);
 
-  const [xaccelData, setXaccelData] = useState({
-    labels: UserData.map((data) => data.time),
-    datasets: [
-      {
-        label: "Flight Data X Acceleration",
-        data: UserData.map((data) => data.xaccel),
-        backgroundColor: [
-          "rgba(75,192,192,1)",
-          "#ecf0f1",
-          "#50AF95",
-          "#f3ba2f",
-          "#2a71d0",
-        ],
-        borderColor: "black",
-        borderWidth: 2,
-      },
-    ],
-  });
-
-  const [yaccelData, setYaccelData] = useState({
-    labels: UserData.map((data) => data.time),
-    datasets: [
-      {
-        label: "Flight Data Y Acceleration",
-        data: UserData.map((data) => data.yaccel),
-        backgroundColor: [
-          "rgba(75,192,192,1)",
-          "#ecf0f1",
-          "#50AF95",
-          "#f3ba2f",
-          "#2a71d0",
-        ],
-        borderColor: "black",
-        borderWidth: 2,
-      },
-    ],
-  });
-
-
-  const [zaccelData, setZaccelData] = useState({
-    labels: UserData.map((data) => data.time),
-    datasets: [
-      {
-        label: "Flight Data Z Acceleration",
-        data: UserData.map((data) => data.zaccel),
-        backgroundColor: [
-          "rgba(75,192,192,1)",
-          "#ecf0f1",
-          "#50AF95",
-          "#f3ba2f",
-          "#2a71d0",
-        ],
-        borderColor: "black",
-        borderWidth: 2,
-      },
-    ],
-  });
-
-  const [elevatorData, setElevatoraccelData] = useState({
-    labels: UserData.map((data) => data.time),
-    datasets: [
-      {
-        label: "Flight Data Elevator",
-        data: UserData.map((data) => data.elevator),
-        backgroundColor: [
-          "rgba(75,192,192,1)",
-          "#ecf0f1",
-          "#50AF95",
-          "#f3ba2f",
-          "#2a71d0",
-        ],
-        borderColor: "black",
-        borderWidth: 2,
-      },
-    ],
-  });
-
-  const [aileronData, setAileronaccelData] = useState({
-    labels: UserData.map((data) => data.time),
-    datasets: [
-      {
-        label: "Flight Data Aileron",
-        data: UserData.map((data) => data.aileron),
-        backgroundColor: [
-          "rgba(75,192,192,1)",
-          "#ecf0f1",
-          "#50AF95",
-          "#f3ba2f",
-          "#2a71d0",
-        ],
-        borderColor: "black",
-        borderWidth: 2,
-      },
-    ],
-  });
-
-    const [rudderData, setRudderData] = useState({
-    labels: UserData.map((data) => data.time),
-    datasets: [
-      {
-        label: "Flight Data Rudder",
-        data: UserData.map((data) => data.rudder),
-        backgroundColor: [
-          "rgba(75,192,192,1)",
-          "#ecf0f1",
-          "#50AF95",
-          "#f3ba2f",
-          "#2a71d0",
-        ],
-        borderColor: "black",
-        borderWidth: 2,
-      },
-    ],
-  });
-
-
+  const highestValues = findHighestValues(UserData);
 
   return (
     <div className="App">
       <h1>Flight Data Charts</h1>
-
       <div>
-        <p>Flight Duration: {highestValues.time}s</p>
-        <p>Max Altitude:  {highestValues.altitude}</p>
-        <p>Max xaccel:  {highestValues.xaccel}</p>
-        <p>Max yaccel:  {highestValues.yaccel}</p>
-        <p>Max zaccel:  {highestValues.zaccel}</p>
-        <p>Max elevator:  {highestValues.elevator}°</p>
-        <p>Max aileron: {highestValues.aileron}°</p>
-        <p>Max rudder:  {highestValues.rudder}°</p>
+        {Object.entries(highestValues).map(([key, value]) => (
+          <p key={key}>Max {key}: {value}{['elevator', 'aileron', 'rudder'].includes(key) ? '°' : ''}</p>
+        ))}
       </div>
-
-
-      <div style={{ width: 700 }}>
-        <LineChart chartData={altitudeData} />
-      </div>
-
-      <div style={{ width: 700 }}>
-        <LineChart chartData={xaccelData} /> 
-      </div>
-      
-      <div style={{ width: 700 }}>
-        <LineChart chartData={yaccelData} /> 
-      </div>
-
-      <div style={{ width: 700 }}>
-        <LineChart chartData={zaccelData} /> 
-      </div>
-
-      <div style={{ width: 700 }}>
-        <LineChart chartData={elevatorData} /> 
-      </div>
-
-      <div style={{ width: 700 }}>
-        <LineChart chartData={aileronData} /> 
-      </div>
-
-      <div style={{ width: 700 }}>
-        <LineChart chartData={rudderData} /> 
-      </div>
-
+      {chartsInfo.map(({ key }) => chartData[key] && (
+        <div key={key} style={{ width: 700 }}>
+          <LineChart chartData={chartData[key]} />
+        </div>
+      ))}
     </div>
   );
 }
 
 export default App;
+
